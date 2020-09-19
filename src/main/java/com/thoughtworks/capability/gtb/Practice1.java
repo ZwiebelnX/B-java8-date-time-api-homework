2974e371c46253021354f48e6531d188b0b159b8
Chen Sicong <sicong.chen@163.com>
2020-09-19 21:26:56 +0800

    [Chen Sicong] resolved problem1

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -1,6 +1,9 @@
 package com.thoughtworks.capability.gtb;
 
 import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 
 /**
  * 计算任意日期与下一个劳动节相差多少天
@@ -11,6 +14,10 @@ import java.time.LocalDate;
 public class Practice1 {
 
   public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
+    LocalDate laborDay = LocalDate.now().withMonth(5).withDayOfMonth(1);
+    if (laborDay.isBefore(date)) {
+      laborDay = laborDay.plus(1, ChronoUnit.YEARS);
+    }
+    return laborDay.toEpochDay() - date.toEpochDay();
   }
 }
